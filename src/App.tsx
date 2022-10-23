440575a8a1ab21fc29f65cad961acca5b2ac960b
ktpi2000 <katapi2000@gmail.com>
2022-10-23 17:16:04 +0900

    ざーーーっとしあげる

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import { MantineProvider, Text } from '@mantine/core';
+import { Container, MantineProvider, Text, Title } from '@mantine/core';
+import { About } from './components/About';
+import { History } from './components/History';
+import { Introduction } from './components/Introduction';
+import { Links } from './components/Links';
 
 const App = () => (
   <MantineProvider withGlobalStyles withNormalizeCSS>
-    <Text>Welcome to Mantine!</Text>
+    <Container>
+      <div style={{ marginTop: '2rem' }}>
+        <Introduction />
+      </div>
+
+      <div style={{ padding: '1rem 0rem' }}>
+        <Title order={2}>about</Title>
+        <About />
+      </div>
+
+      <div style={{ padding: '1rem 0rem' }}>
+        <Title order={2}>links</Title>
+        <Links />
+      </div>
+
+      <div style={{ padding: '1rem 0rem' }}>
+        <Title order={2}>skills</Title>
+        <Text>
+          React / Next.js / Vue.js / Nuxt.js / TypeScript / Emotion / Styled-component / Firebase
+        </Text>
+      </div>
+
+      <div style={{ padding: '1rem 0rem' }}>
+        <Title order={2}>history</Title>
+        <History />
+      </div>
+    </Container>
   </MantineProvider>
 );
 
